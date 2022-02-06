033353feae392cadbce043262a34370ec8ae2237
Ramsay Sewell <contact@ramsay.me>
2022-02-06 18:37:59 +0000

    initial teams page setup

--- src/pages/TeamPage.js
+++ src/pages/TeamPage.js
@@ -1,13 +1,32 @@
+import {Container, Grid, Typography} from "@mui/material";
 import React from "react";
+import Jumbotron from "../components/JumbotronComponent";
+import {team} from "../data/team.json";
 
 const Team = () => {
-
-    return(
-        <div>
-        <h1>Teams</h1>
-        </div>
-    );
-
-}
+  return (
+    <>
+      <Jumbotron title='Team' />
+      <Container sx={{maxWidth: 1200, margin: "2rem auto"}}>
+        <Grid container spacing={2}>
+          {Object.values(team).length > 0 &&
+            Object.values(team).map((member) => {
+              return (
+                <Grid item xs={6} key={member.id} component='li'>
+                  <Typography
+                    variant='h6'
+                    sx={{display: "inline-block", marginRight: "1rem"}}
+                  >
+                    {member.name}
+                  </Typography>
+                  <Typography variant='span'>{member.dateJoined}</Typography>
+                </Grid>
+              );
+            })}
+        </Grid>
+      </Container>
+    </>
+  );
+};
 
 export default Team;
