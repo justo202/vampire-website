111a044c9ce9053141a30f65b6cbb50d9d858e73
justo202 <justaslabeikis23@gmail.com>
2022-03-22 17:59:44 +0200

    Changed research design

---
+++ src/components/researchComponent.js
@@ -0,0 +1,135 @@
+import React from "react";
+import { Box, Grid, Link, Typography } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles(() => {
+  return {
+    researchImage: {
+      objectFit: "cover",
+      width: "100%",
+      height: "100%",
+    },
+    imageContainer: {
+      width: "100%",
+      height: "60%",
+    },
+  };
+});
+
+const CaptionSection = ({ funding, timeframe }) => {
+  return (
+    <Box mb={1}>
+      <Typography
+        pr={2}
+        sx={{ fontWeight: "bolder" }}
+        variant="caption"
+        gutterBottom
+      >
+        Time Frame:{" "}
+        <span style={{ fontWeight: "normal" }}>
+          {timeframe[0]}-{timeframe[1]}
+        </span>
+      </Typography>
+      <Typography sx={{ fontWeight: "bolder" }} variant="caption" gutterBottom>
+        Supporting grants:{" "}
+        <span style={{ fontWeight: "normal" }}>{funding}</span>
+      </Typography>
+    </Box>
+  );
+};
+const SectionPointers = ({ pointers }) => {
+  const mapPointers = pointers.map((pointer) => (
+    <Link
+      display={"block"}
+      href={pointer.link}
+      variant="body1"
+      gutterBottom
+      underline="hover"
+      color={"accent.main"}
+    >
+      {pointer.title}
+    </Link>
+  ));
+
+  return mapPointers;
+};
+
+const TextSection = ({
+  title,
+  funding,
+  timeframe,
+  description,
+  collaborators,
+}) => {
+  return (
+    <Grid xs={7} item>
+      <Typography variant="h5" gutterBottom>
+        {title}
+      </Typography>
+      <CaptionSection funding={funding} timeframe={timeframe} />
+      <Typography mb={2} variant="body1">
+        <span style={{ fontWeight: "bold" }}>Description: </span>
+        {description}
+      </Typography>
+      <Typography variant="body2">
+        <span style={{ fontWeight: "bold" }}>Collaborators: </span>
+        {collaborators.join(", ")}
+      </Typography>
+    </Grid>
+  );
+};
+const ImageSection = ({ styles, photo, pointers }) => {
+  return (
+    <Grid xs={5} item alignItems={"center"}>
+      <Box className={styles.imageContainer} mb={1} sx={{ boxShadow: 3 }}>
+        <img className={styles.researchImage} alt="research" src={photo} />
+      </Box>
+      <Typography variant="body1" textAlign={"center"}>
+        Publication pointers
+      </Typography>
+      <SectionPointers pointers={pointers} />
+    </Grid>
+  );
+};
+
+const ResearchSection = ({
+  title,
+  description,
+  funding = "",
+  timeframe = ["", ""],
+  photo,
+  collaborators = [],
+  pointers = [],
+  imageLeft = false,
+}) => {
+ 
+  const styles = useStyles();
+  return (
+    <Grid container spacing={2}>
+      {imageLeft ? (
+        <ImageSection styles={styles} photo={photo} pointers={pointers} />
+      ) : (
+        <TextSection
+          title={title}
+          description={description}
+          funding={funding}
+          timeframe={timeframe}
+          collaborators={collaborators}
+        />
+      )}
+      {imageLeft ? (
+        <TextSection
+          title={title}
+          description={description}
+          funding={funding}
+          timeframe={timeframe}
+          collaborators={collaborators}
+        />
+      ) : (
+        <ImageSection styles={styles} photo={photo} pointers={pointers} />
+      )}
+    </Grid>
+  );
+};
+
+export default ResearchSection;
--- src/pages/Research.js
+++ src/pages/Research.js
@@ -1,138 +1,103 @@
 import React from "react";
 import Jumbotron from "../components/JumbotronComponent";
-import { Grid, Paper, Typography } from "@mui/material";
-import { makeStyles, useTheme } from "@mui/styles";
-
-const useStyles = makeStyles((theme) => {
-    return {
-      researchImages: {
-        width: '100%',
-        height: '100%'
-      },
-      textBackground: {
-        backgroundColor: theme.palette.darkBackground.main
-      }
-    }
-})
-  
-const ResearchItem = ({imgUrl, description, title, even}) => {
-    const theme = useTheme();
-    const styles = useStyles(theme);
-   
-    if(even) {
-        return (
-            <>
-            <Grid item xs={12} md={8}>
-            <Paper sx={{backgroundColor: theme.palette.lightBackground.main}}>
-                <Grid container>
-                <Grid item xs={12} sm={5}>
-                <img className={styles.researchImages} src={imgUrl} alt='Research'/>
-            </Grid>
-            <Grid item xs={12} sm={7} padding={10}>             
-                <Typography
-                variant="h5"
-                >
-                    {title}
-                </Typography>
-                <Typography variant="p" >
-                    {description}
-                </Typography>
-            </Grid>
-                </Grid>
-            
-            </Paper>
-            </Grid>
-            </>
-        );
-    } else {
-        return(
-            <>
-            <Grid item xs={12} md={8}>
-                <Paper sx={{backgroundColor: theme.palette.lightBackground.main}}>
-                    <Grid container>
-                        <Grid item xs={12} sx={{display: { xs: 'flex', sm: 'none' } }}>
-                             <img className={styles.researchImages} src={imgUrl} alt='Research'/>
-                        </Grid>
-                        <Grid item xs={12} sm={7} padding={10}>
-                            <Typography
-                            variant="h5"
-                            >
-                                {title}
-                            </Typography>
-                            <Typography variant="p" >
-                                {description}
-                            </Typography>
-                      </Grid>
-                        <Grid item xs={12} sm={5} sx={{display: { xs: 'none', sm: 'flex' } }}>
-                             <img className={styles.researchImages} src={imgUrl} alt='Research'/>
-                        </Grid>                  
-                    </Grid>
-                </Paper>
-            </Grid>
-            </>
-        )
-    }
-
-}
-
-
+import { Grid } from "@mui/material";
+import ResearchSection from "../components/researchComponent";
 
 const Research = () => {
-    const researchList = [
+  const researchList = [
+    {
+      photo: "https://picsum.photos/400",
+      description:
+        "lorem ipsum lorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsum",
+      title: "A research title that can be a bit longer than normal titles",
+      pointers: [
         {
-            imgUrl: 'https://picsum.photos/400',
-            description: 'lorem ipsum lorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsum',
-            title: '1',
-            key: 1
+          title: "Test pointer to w3schools!",
+          link: "https://www.w3schools.com/",
         },
         {
-            imgUrl: 'https://picsum.photos/400',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            title: '2',
-            key: 2
+          title: "Another pointer to another place",
+          link: "https://www.google.com/",
         },
-        {
-            imgUrl: 'https://picsum.photos/400',
-            description: 'lorem ipsum lorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsum',
-            title: '3',
-            key: 3
-        },
-        {
-            imgUrl: 'https://picsum.photos/400',
-            description: 'lorem ipsum lorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsum',
-            title: '4',
-            key: 4
-        },
-        {
-            imgUrl: 'https://picsum.photos/400',
-            description: 'lorem ipsum lorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsum',
-            title: '5',
-            key: 5
-        },
-        {
-            imgUrl: 'https://picsum.photos/400',
-            description: 'lorem ipsum lorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsum',
-            title: '6',
-            key: 6
-        }
-    ]
-    var isEven = false;
-    const researchListMap = researchList.map(item => {
-        isEven = !isEven
-        return(
-        <ResearchItem even={isEven} imgUrl={item.imgUrl} description={item.description} title={item.title} key={item.key}/>
-        )
-    })
-
-    return(
-        <>
-        <Jumbotron  title={'Research'} subtitle={'Research projects done in VAMPIRE'}/>
-        <Grid container alignItems={'center'} mr={'auto'} ml={"auto"} direction={"column"} spacing={0} maxWidth={'1100px'}>
-            {researchListMap}
-        </Grid>
-        </>
+      ],
+      collaborators: ['Person', 'Another person', 'Third person', 'Fourth person'],
+      timeframe: ["2021", "2022"],
+      funding: "Colaborator",
+      key: 1,
+    },
+    {
+      photo: "https://picsum.photos/400",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      title: "2",
+      key: 2,
+    },
+    {
+      photo: "https://picsum.photos/400",
+      description:
+        "lorem ipsum lorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsum",
+      title: "3",
+      key: 3,
+    },
+    {
+      photo: "https://picsum.photos/400",
+      description:
+        "lorem ipsum lorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsum",
+      title: "4",
+      key: 4,
+    },
+    {
+      photo: "https://picsum.photos/400",
+      description:
+        "lorem ipsum lorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsum",
+      title: "5",
+      key: 5,
+    },
+    {
+      photo: "https://picsum.photos/400",
+      description:
+        "lorem ipsum lorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsumlorem ipsum lorem ipsum",
+      title: "6",
+      key: 6,
+    },
+  ];
+  var left = true;
+  const researchListMap = researchList.map((item) => {
+    left = !left;
+    return (
+      <ResearchSection
+        imageLeft={left}
+        description={item.description}
+        title={item.title}
+        photo={item.photo}
+        pointers={item.pointers}
+        collaborators={item.collaborators}
+        timeframe={item.timeframe}
+        funding={item.funding}
+      />
     );
+  });
 
-}
+  return (
+    <>
+      <Jumbotron
+        title={"Research"}
+        subtitle={"Research projects done in VAMPIRE"}
+      />
+      <Grid
+        container
+        alignItems={"center"}
+        mr={"auto"}
+        ml={"auto"}
+        mt={1}
+        direction={"column"}
+        rowGap={1}
+        maxWidth={"1100px"}
+      >
+        {researchListMap}
+      </Grid>
+    </>
+  );
+};
 
 export default Research;
